3cae11c412532cd996c1f8f362d8d9140f542a6b
HYEON <titeotty12@gmail.com>
2025-06-06 01:45:54 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/dasom/MemoReal/MemoRealApplication.java
@@ -0,0 +1,13 @@
+package com.dasom.MemoReal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasom/MemoReal/MemoRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasom.MemoReal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
