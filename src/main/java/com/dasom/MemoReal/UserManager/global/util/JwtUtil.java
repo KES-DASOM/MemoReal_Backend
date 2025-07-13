31f8c6ee68fa87b6df3922dfcd550215b82148cb
junjinyun <kjkj4142@naver.com>
2025-07-13 21:27:59 +0900

    feat : jwt 관리 코드 업로드

---
+++ src/main/java/com/dasom/MemoReal/UserManager/global/util/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.dasom.MemoReal.UserManager.global.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 86400000L; // 1일
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public static String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
