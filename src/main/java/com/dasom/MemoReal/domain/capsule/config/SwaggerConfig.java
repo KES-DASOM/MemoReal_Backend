11450e83e406ea341b5cdd3cb958cbe02a8fe330
si-zero <snm306@naver.com>
2025-07-21 00:40:50 +0900

    feat: 스웨거 콘피그 세팅

---
+++ src/main/java/com/dasom/MemoReal/domain/capsule/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.dasom.MemoReal.domain.capsule.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("API 문서")
+                        .description("Swagger를 활용한 API 문서입니다.")
+                        .version("1.0.0")
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
